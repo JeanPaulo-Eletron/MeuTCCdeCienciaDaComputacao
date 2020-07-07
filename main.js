9afc0c19a64ffcade7d05e032c0753ff015898c2
JeanPauloRioPreto@GMail.Com <Jean Paulo Athanazio De Mei>
2020-07-06 23:40:23 -0300

    no message

---
+++ main.js
@@ -0,0 +1,16 @@
+var BtnEnviar   = document.querySelector('#BtnEnviar')
+CaixaDeResposta = document.querySelector('#CaixaDeResposta')
+if (window.Worker) {
+    var myWorker = new Worker('ThreadWork.js');
+	BtnEnviar.onclick = ()=>{
+        myWorker.postMessage(CaixaDeTexto.value)
+        console.log('Message posted to worker');
+    }
+
+	myWorker.onmessage = function(e) {
+        CaixaDeResposta.textContent += e.data + "\n";
+		console.log('Message received from worker'+e.data);
+	}
+} else {
+	console.log('Your browser doesn\'t support web workers.')
+}
