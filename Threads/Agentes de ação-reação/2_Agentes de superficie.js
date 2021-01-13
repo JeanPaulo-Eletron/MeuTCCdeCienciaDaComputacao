f8eecbd0ed5b1773e77231426d64d1cba4db0c5e
Jean Paulo Athanazio De Mei <61103868+JeanPaulo-Eletron@users.noreply.github.com>
2021-01-13 10:01:07 -0300

    Delete Threads directory

--- Threads/Agentes de ação-reação/2_Agentes de superficie.js
+++
@@ -1,6 +0,0 @@
-importScripts('Agentes de ação-reação/3_Agentes Atores.js');
-
-const AgenteAcaoReacaoDeFocoObserver = new Observable();
-
-AgenteAcaoReacaoDeFocoObserver.subscribe(()=>{DelegadoAgenteOi.callAgent()});
-AgenteAcaoReacaoDeFocoObserver.subscribe(()=>{DelegadoAgenteTudo.callAgent()});
